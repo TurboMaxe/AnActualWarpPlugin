bbe1131200534a9181c5f20189be44b14ef5320c
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-03 20:40:30 +0500

    cleanup: remove getter method and add lombok, also cleaned up code

--- bukkit/src/main/java/me/turbo/random/AnActualWarpPlugin/CommandHandler.java
+++ bukkit/src/main/java/me/turbo/random/AnActualWarpPlugin/CommandHandler.java
@@ -3,37 +3,32 @@ package me.turbo.random.anActualPlugin1;
 import me.turbo.random.anActualPlugin1.commands.DelWarp;
 import me.turbo.random.anActualPlugin1.commands.SetWarp;
 import me.turbo.random.anActualPlugin1.commands.Warp;
+import lombok.Getter;
 import me.turbo.random.anActualPlugin1.commands.Warps;
-import org.bukkit.configuration.file.FileConfiguration;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandHandler extends JavaPlugin {
-
-
-    private static CommandHandler instance;
+    @Getter private static CommandHandler instance;
 
     @Override
     public void onEnable() {
-
         instance = this;
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new EventListener(), this);
-        getLogger().info("Plugin enabled!");
+        getLogger().info("AAWP enabled!");
 
         getCommand("warp").setExecutor(new Warp(this));
         getCommand("setwarp").setExecutor(new SetWarp(this));
         getCommand("warps").setExecutor(new Warps());
         getCommand("delwarp").setExecutor(new DelWarp());
 
-        FileConfiguration config = this.getConfig();
     }
-        public static CommandHandler getInstance() {
-            return instance;
-        }
-
+    
     @Override
     public void onDisable() {
-        getLogger().info("Plugin disabled!");
+        getLogger().info("AAWP disabled!");
 
     }
 }
+
